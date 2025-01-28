8f07c03eec7d3d08e98bf67c241a609d276a4435
kirantj4197 <kirantj4197@gmail.com>
2025-01-28 07:20:11 +0530

    new page object

---
+++ SeleniumE2E/src/test/java/pageObject/AllElementsPage.java
@@ -0,0 +1,32 @@
+package pageObject;
+
+import java.time.Duration;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AllElementsPage extends BasePage {
+	
+	
+
+	WebDriver driver;
+	
+	public AllElementsPage(WebDriver dirver) {
+		super(dirver);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+	
+	
+	
+	
+
+	
+	
+}
